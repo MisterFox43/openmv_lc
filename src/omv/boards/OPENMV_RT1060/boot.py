ce21755ac417f642b0059db476817ee0646377f3
MisterFox43 <141587443+MisterFox43@users.noreply.github.com>
2024-09-14 18:31:33 +0200

    Update boot.py

--- src/omv/boards/OPENMV_RT1060/boot.py
+++ src/omv/boards/OPENMV_RT1060/boot.py
@@ -1,7 +1,6 @@
-
-# main.py -- put your code here!"
 import machine, time
-led = machine.LED(\"LED_BLUE\")
+led = machine.LED("LED_RED")
+
 while (True):
    led.on()
    time.sleep_ms(550)
@@ -11,4 +10,3 @@ while (True):
    time.sleep_ms(550)
    led.off()
    time.sleep_ms(600)
-
