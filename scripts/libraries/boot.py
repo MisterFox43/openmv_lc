2d198205866e3bd5f12d61a6da99d25e027a6a01
MisterFox43 <141587443+MisterFox43@users.noreply.github.com>
2024-09-14 18:46:33 +0200

    Add files via upload

--- scripts/libraries/boot.py
+++ scripts/libraries/boot.py
@@ -1,13 +1,12 @@
-# main.py -- put your code here!\n
-import machine, time\n
-led = machine.LED(\"LED_RED\")\n
-while (True):\n
-   led.on()\n
-   time.sleep_ms(550)\n
-   led.off()\n
-   time.sleep_ms(400)\n
-   led.on()\n
-   time.sleep_ms(550)\n
-   led.off()\n
-   time.sleep_ms(600)\n
-;
+import machine,
+import time
+led = machine.LED("LED_RED")
+while (True):
+   led.on()
+   time.sleep_ms(550)
+   led.off()
+   time.sleep_ms(400)
+   led.on()
+   time.sleep_ms(550)
+   led.off()
+   time.sleep_ms(600)
