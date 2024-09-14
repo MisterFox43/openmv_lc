75c878d26e60671f4ba73fab06cc6028bcdb40eb
MisterFox43 <141587443+MisterFox43@users.noreply.github.com>
2024-09-14 18:26:17 +0200

    Update boot.py

--- src/omv/boards/OPENMV_RT1060/boot.py
+++ src/omv/boards/OPENMV_RT1060/boot.py
@@ -1,14 +1,14 @@
-static const char fresh_main_py[] =
-    "# main.py -- put your code here!\n"
-    "import machine, time\n"
-    "led = machine.LED(\"LED_BLUE\")\n"
-    "while (True):\n"
-    "   led.on()\n"
-    "   time.sleep_ms(550)\n"
-    "   led.off()\n"
-    "   time.sleep_ms(400)\n"
-    "   led.on()\n"
-    "   time.sleep_ms(550)\n"
-    "   led.off()\n"
-    "   time.sleep_ms(600)\n"
-;
+
+# main.py -- put your code here!"
+import machine, time
+led = machine.LED(\"LED_BLUE\")
+while (True):
+   led.on()
+   time.sleep_ms(550)
+   led.off()
+   time.sleep_ms(400)
+   led.on()
+   time.sleep_ms(550)
+   led.off()
+   time.sleep_ms(600)
+
